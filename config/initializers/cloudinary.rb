927dd0ca5f30ec120587ca7d99f16b481c908526
PAxNova <votre.email@example.com>
2024-07-05 11:48:09 +0200

    ok

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,8 @@
 Cloudinary.config do |config|
-    config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-    config.api_key = ENV['CLOUDINARY_API_KEY']
-    config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    config.cloud_name = ENV['CLOUDINARY_URL'].split('@').last
+    config.api_key = ENV['CLOUDINARY_URL'].split('://').last.split(':').first
+    config.api_secret = ENV['CLOUDINARY_URL'].split(':').last.split('@').first
     config.secure = true
-    config.cdn_subdomain = true
-end  
+    config.cdn_subdomain = true  
+  end
+  
