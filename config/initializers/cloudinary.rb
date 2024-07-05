9cd29a59ad4eb390248393849f78231d06337cee
PAxNova <votre.email@example.com>
2024-07-05 11:37:32 +0200

    update the cloudinary setup

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,7 @@
 Cloudinary.config do |config|
-    config.cloud_name = ENV['CLOUDINARY_URL']
-  end
-  
+    config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+    config.api_key = ENV['CLOUDINARY_API_KEY']
+    config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    config.secure = true
+    config.cdn_subdomain = true
+end  
